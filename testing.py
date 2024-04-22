f047b6f4cc15f18ade61ae3286944cf8a8904ae8
DOYE OGHENETEJIRI <doyetejiri@gmail.com>
2024-04-22 17:40:20 +0100

    corrected error in testing.py

--- testing.py
+++ testing.py
@@ -1,8 +1,9 @@
-from testing import age_calculation
+from age_func import age_calculation
 
 first_name = "Jude"
 last_name = "James"
 age = 98
+is_adult = True
 print(first_name)
 print('my name is ' + first_name + ' ' + last_name)
 
